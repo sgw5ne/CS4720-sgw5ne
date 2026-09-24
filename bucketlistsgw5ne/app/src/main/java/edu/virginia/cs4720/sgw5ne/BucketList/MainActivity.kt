ab97dc89066a743e895d3776c68ae49d74cabfcb
William Lee <sgw5ne@virginia.edu>
2026-09-24 12:04:45 -0400

    setting up for assignment 1

---
+++ bucketlistsgw5ne/app/src/main/java/edu/virginia/cs4720/sgw5ne/BucketList/MainActivity.kt
@@ -0,0 +1,58 @@
+package edu.virginia.cs4720.sgw5ne.BucketList
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import edu.virginia.cs4720.sgw5ne.BucketList.ui.theme.BucketLIstTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            BucketLIstTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+class ListActivityViewModel : ViewModel() {
+    // Launch screen vm
+}
+
+@Composable
+fun ListActivityScreen(vm: ListActivityViewModel = viewModel()) {
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BucketLIstTheme {
+        Greeting("Android")
+    }
+}
